6d7204f659974d2d653a2a12307d71f5599dbf91
TheFatPanda97 <shawnhu97@gmail.com>
2023-07-07 19:57:16 -0400

    working on add history

--- frontend/src/components/AnswerDisplay/index.tsx
+++ frontend/src/components/AnswerDisplay/index.tsx
@@ -1,3 +1,8 @@
+import { useState } from 'react';
+import { BsClockHistory } from 'react-icons/bs';
+import classNames from 'classnames';
+import { StaticMathField } from 'react-mathquill';
+
 import type { FC } from 'react';
 import './index.scss';
 
@@ -6,10 +11,35 @@ interface IProps {
 }
 
 const AnswerDisplay: FC<IProps> = ({ answer }) => {
+  const [showHistory, setShowHistory] = useState(false);
+
   return (
-    <div className="answer-display">
-      {!answer.includes('Invalid Input') && <span>Answer:&nbsp;</span>}
-      <p>{answer}</p>
+    <div
+      className={classNames('answer-display-container', {
+        'answer-display-container--expanded': showHistory,
+      })}
+    >
+      <div className="answer-display">
+        <div className="answer-container">
+          {!answer.includes('Invalid Input') && <span>Answer:&nbsp;</span>}
+          <p>{answer}</p>
+        </div>
+        <div className="history-btn" onClick={() => setShowHistory(!showHistory)}>
+          <BsClockHistory />
+        </div>
+      </div>
+      {showHistory && (
+        <div className="history-container">
+          <hr />
+          <div className="past-equation-container">
+            <StaticMathField className="past-equation">
+              {'\\frac{1}{\\sqrt{2}}\\cdot 2'}
+            </StaticMathField>
+            =<div className="past-answer">56456465456456</div>
+            <div className="past-variables">x = 3, y = 10</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
