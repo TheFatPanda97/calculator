884b97fd61d595b00e4f9dd82188177508ee6f05
TheFatPanda97 <shawnhu97@gmail.com>
2023-07-07 22:57:48 -0400

    added sqlite3 support

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,41 @@
+import 'dotenv/config';
 import express from 'express';
+import {
+  insertEquation as insertEquationLite,
+  getEquations as getEquationsLite,
+} from './util/sqliteDAO';
+
 import type { Express, Request, Response } from 'express';
 
 const app: Express = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.use(express.json());
+
+app.get('/', (_, res: Response) => {
+  res.send('Pretty Calculator API');
+});
+
+app.get('/equations', (_, res: Response) => {
+  if (process.env.DB_TYPE === 'sqlite3') {
+    getEquationsLite(res);
+  }
+});
+
+app.post('/equations', (req: Request, res: Response) => {
+  if (!req.body) return res.sendStatus(400);
+
+  const { latex, answer, assignments } = req.body;
+
+  if (!latex || !answer || !assignments) {
+    return res.sendStatus(400);
+  }
+
+  if (process.env.DB_TYPE === 'sqlite3') {
+    insertEquationLite(res, latex, answer, assignments);
+  }
 });
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[Calculator API] is running on port ${port}`);
 });
---
+++ backend/util/sqliteDAO.ts
@@ -0,0 +1,49 @@
+import * as sqlite3 from 'sqlite3';
+
+const core = sqlite3.verbose();
+const db = new core.Database('./db/calculator.db');
+
+import type { Response } from 'express';
+
+export const insertEquation = (
+  res: Response,
+  latex: string,
+  answer: string,
+  assignments: Record<string, number | ''>,
+) =>
+  db.run(
+    `INSERT INTO calculations(latex, answer, assignments) VALUES(?, ?, ?)`,
+    [latex, answer, JSON.stringify(assignments)],
+    (err) => {
+      if (err) {
+        res.sendStatus(500);
+      } else {
+        res.send('Equation Added');
+      }
+    },
+  );
+
+export const getEquations = (res: Response) => {
+  interface IROW {
+    latex: string;
+    answer: string;
+    assignments: string;
+  }
+
+  db.all(
+    `SELECT latex, answer, assignments FROM calculations ORDER BY created_at DESC`,
+    (err, rows: IROW[]) => {
+      if (err) {
+        res.sendStatus(500);
+      } else {
+        const equations = rows.map((row) => ({
+          latex: row.latex,
+          answer: row.answer,
+          assignments: JSON.parse(row.assignments),
+        }));
+
+        res.send(equations);
+      }
+    },
+  );
+};
