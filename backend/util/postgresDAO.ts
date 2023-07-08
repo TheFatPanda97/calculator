03c330835c136c69921ecf47e5bb0642a5d92433
TheFatPanda97 <shawnhu97@gmail.com>
2023-07-07 23:51:16 -0400

    working on ssl for psql

--- backend/util/postgresDAO.ts
+++ backend/util/postgresDAO.ts
@@ -6,7 +6,7 @@ const db = new Client({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWD,
-  ssl: true,
+  ssl: process.env.NODE_ENV === 'development',
 });
 
 export const connectDB = async () => {
